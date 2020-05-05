70565d6552695e1c47585e0a967051775ebc47c2
Yinan Zhu <zynsteve@tamu.edu>
2020-05-04 22:15:18 -0500

    implemented decorator pattern

---
+++ src/decorator/Beverage.java
@@ -0,0 +1,11 @@
+package decorator;
+
+public abstract class Beverage {
+    String description = "Unknow Beverage";
+
+    public String getDescription() {
+        return description;
+    }
+
+    public abstract double cost();
+}
---
+++ src/decorator/StarbuzzCoffee.java
@@ -0,0 +1,12 @@
+package decorator;
+
+public class StarbuzzCoffee {
+    public static void main(String[] args) {
+        Beverage beverage = new Espresso();
+        System.out.println(beverage.getDescription() + " $" + beverage.cost());
+
+        beverage = new Mocha(beverage);
+        beverage = new Mocha(beverage);
+        System.out.println(beverage.getDescription() + " $" + beverage.cost());
+    }
+}
